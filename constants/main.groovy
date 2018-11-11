a2a3c4b73d0f836595529baac13ba3527976cb3f
masaya.toga <masaya.toga@ari-jp.com>
2018-11-12 00:36:56 +0900

    修正

--- constants/main.groovy
+++ constants/main.groovy
@@ -1,44 +1,54 @@
-/*
-定数を定義
-大きな機能ごとにパブリックなclassを定義し、
-classに変更不可の静的な変数を定義する。
+// /*
+// 定数を定義
+// 大きな機能ごとにパブリックなclassを定義し、
+// classに変更不可の静的な変数を定義する。
 
-使用例)
+// 使用例)
 
-constants/main.groovy
------------------------------
-public class SAMPLE {
-  public static final NUM = 100
-}
+// constants/main.groovy
+// -----------------------------
+// public class SAMPLE {
+//   public static final NUM = 100
+// }
 
-Jenkinsfile
------------------------------
+// Jenkinsfile
+// -----------------------------
 
-pipeline {
-  agent any
-  stages {
-    stage("インポート定数") {
-      steps {
-        script {
-          def CONST = load("${pwd()}/constants/main.groovy")
-          echo CONST.SAMPLE.NUM
-        }
-      }
-    }
-  }
-}
+// pipeline {
+//   agent any
+//   stages {
+//     stage("インポート定数") {
+//       steps {
+//         script {
+//           def CONST = load("${pwd()}/constants/main.groovy")
+//           echo CONST.SAMPLE.NUM
+//         }
+//       }
+//     }
+//   }
+// }
 
-*/
+// */
 
-// Slackに関する定数の定義
-public class Slack {
-  // Slackのチャンネルと認証IDの紐づけ情報
-  public static final CHANNEL_CREDENTIAL_IDS = [
-    "#hooktest": "approval_slack_token",
+// // Slackに関する定数の定義
+// public class Slack {
+//   // Slackのチャンネルと認証IDの紐づけ情報
+//   public static final CHANNEL_CREDENTIAL_IDS = [
+//     "#hooktest": "approval_slack_token",
+//   ]
+// }
+
+// def num = 10
+// def SLACK = Slack
+
+def CONST() {
+  return [
+    'SLACK': [
+      'CHANNEL_CREDENTIAL_IDS': [
+        '#hooktest': 'approval_slack_token'
+      ]
+    ]
   ]
 }
 
-def num = 10
-def SLACK = Slack
-
-return this
+return this.CONST()
--- lib/slack.groovy
+++ lib/slack.groovy
@@ -1,5 +1,5 @@
 def notify_message(channel, message) {
-  def CONST = load("${pwd()}/constants/main.groovy")
+  def CONST = load("constants/main.groovy")
   def credentialsId = CONST.SLACK.CHANNEL_CREDENTIAL_IDS[channel]
 
   withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
