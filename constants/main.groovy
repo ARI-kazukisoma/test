f4700b9f8bef478a5c10c888d561f59585e45135
masaya.toga <masaya.toga@ari-jp.com>
2018-11-12 01:01:39 +0900

    修正

--- constants/main.groovy
+++ constants/main.groovy
@@ -1,45 +1,25 @@
-// /*
-// 定数を定義
-// 大きな機能ごとにパブリックなclassを定義し、
-// classに変更不可の静的な変数を定義する。
+/*
+定数を定義
 
-// 使用例)
+使用例)
 
-// constants/main.groovy
-// -----------------------------
-// public class SAMPLE {
-//   public static final NUM = 100
-// }
+Jenkinsfile
+-----------------------------
 
-// Jenkinsfile
-// -----------------------------
-
-// pipeline {
-//   agent any
-//   stages {
-//     stage("インポート定数") {
-//       steps {
-//         script {
-//           def CONST = load("${pwd()}/constants/main.groovy")
-//           echo CONST.SAMPLE.NUM
-//         }
-//       }
-//     }
-//   }
-// }
-
-// */
-
-// // Slackに関する定数の定義
-// public class Slack {
-//   // Slackのチャンネルと認証IDの紐づけ情報
-//   public static final CHANNEL_CREDENTIAL_IDS = [
-//     "#hooktest": "approval_slack_token",
-//   ]
-// }
-
-// def num = 10
-// def SLACK = Slack
+pipeline {
+  agent any
+  stages {
+    stage("定数を取得") {
+      steps {
+        script {
+          CONSTS = load("constants/main.groovy").get_all()
+          echo CONST.SAMPLE.NUM
+        }
+      }
+    }
+  }
+}
+*/
 
 def get_all() {
   return [
@@ -47,8 +27,7 @@ def get_all() {
       'CHANNEL_CREDENTIAL_IDS': [
         '#hooktest': 'approval_slack_token'
       ]
-    ],
-    'TEST': 'aaa'
+    ]
   ]
 }
 
