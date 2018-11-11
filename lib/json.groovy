88cfd94d763fb306db7ec6865b58e18be2627b7e
masaya.toga <masaya.toga@ari-jp.com>
2018-11-12 01:03:22 +0900

    修正

--- lib/json.groovy
+++ lib/json.groovy
@@ -1,10 +1,11 @@
 def read_json(filePath) {
-  def readJson = read(File(file: filePath))
-  return new JsonSlurperClassic().parseText(readJson)
+  readJson = read(File(file: filePath))
+  data = new JsonSlurperClassic().parseText(readJson)
+  return data
 }
 
 def write_json(data, filePath) {
-  def json = JsonOutPut.toJson(data)
+  json = JsonOutPut.toJson(data)
   writeFile(file: filePath, text: JsonOutPut.prettyPrint(json))
 }
 
