7503fcaeeb8875acda6f1f399816143cc3dc7733
masaya.toga <masaya.toga@ari-jp.com>
2018-11-11 23:25:41 +0900

    修正

--- constants/main.groovy
+++ constants/main.groovy
@@ -1,9 +1,41 @@
-class SLACK {
+/*
+定数を定義
+大きな機能ごとにパブリックなclassを定義し、
+classに変更不可の静的な変数を定義する。
+
+使用例)
+
+constants/main.groovy
+-----------------------------
+public class SAMPLE {
+  public static final NUM = 100
+}
+
+Jenkinsfile
+-----------------------------
+
+pipeline {
+  agent any
+  stages {
+    stage("インポート定数") {
+      steps {
+        script {
+          def CONST = load("${pwd()}/constants/main.groovy")
+          echo CONST.SAMPLE.NUM
+        }
+      }
+    }
+  }
+}
+
+*/
+
+// Slackに関する定数の定義
+public class SLACK {
   // Slackのチャンネルと認証IDの紐づけ情報
-  static final CHANNEL_CREDENTIAL_IDS = [
+  public static final CHANNEL_CREDENTIAL_IDS = [
     "#hooktest": "approval_slack_token",
   ]
 }
 
-echo SLACK.CHANNEL_CREDENTIAL_IDS["#hooktest"]
 return this
