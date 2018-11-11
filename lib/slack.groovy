e9bbaf7ec67a70d9b76c2a3128a4e666557fa53a
masaya.toga <masaya.toga@ari-jp.com>
2018-11-12 00:12:02 +0900

    修正

--- lib/slack.groovy
+++ lib/slack.groovy
@@ -1,7 +1,7 @@
 def notify_message(channel, message) {
   def CONST = load("${pwd()}/constants/main.groovy")
-  // def slack CONST.SLACK()
-  // def credentialsId = CONST.SLACK().CHANNEL_CREDENTIAL_IDS[channel]
+  def slack CONST.SLACK()
+  def credentialsId = CONST.SLACK.CHANNEL_CREDENTIAL_IDS[channel]
 
   // withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
   //   slackSend channel: channel, token: token, message: message
