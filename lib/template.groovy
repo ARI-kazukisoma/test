356a23ea4c8e201197d176de1ceb138760997762
masaya.toga <masaya.toga@ari-jp.com>
2018-11-12 13:37:40 +0900

    修正

---
+++ lib/template.groovy
@@ -0,0 +1,8 @@
+def to_string(filePath, binding) {
+  def f = new File(filePath)
+  def engine = new groovy.text.SimpleTemplateEngine()
+  def template = engine.createTemplate(f).make(binding)
+  return template.toString()
+}
+
+return this
