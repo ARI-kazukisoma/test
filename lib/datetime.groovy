869ee424791973d267896ca70c92196e6e54ec66
masaya.toga <masaya.toga@ari-jp.com>
2018-11-12 16:50:19 +0900

    修正

--- lib/datetime.groovy
+++ lib/datetime.groovy
@@ -5,14 +5,14 @@ TIMEZONEの設定は定数で定義されています。
 
 import java.text.SimpleDateFormat
 
-def string_to_unixtime(dateString, format="yyyy/MM/dd HH:mm:ss") {
+Long string_to_unixtime(dateString, format="yyyy/MM/dd HH:mm:ss") {
   def CONSTS = load("constant/main.groovy").get_all()
   def sdf = new SimpleDateFormat(format)
   sdf.setTimeZone(TimeZone.getTimeZone(CONSTS.TIMEZONE))
   return sdf.parse(dateString).time / 1000
 }
 
-def now(format="yyyy/MM/dd HH:mm:ss") {
+Date now(format="yyyy/MM/dd HH:mm:ss") {
   def CONSTS = load("constant/main.groovy").get_all()
   def date = new Date()
   def sdf = new SimpleDateFormat(format)
@@ -20,7 +20,7 @@ def now(format="yyyy/MM/dd HH:mm:ss") {
   return sdf.format(date)
 
 }
-def string_to_datetime(dateString, format="yyyy/MM/dd HH:mm:ss") {
+Date string_to_datetime(dateString, format="yyyy/MM/dd HH:mm:ss") {
   def CONSTS = load("constant/main.groovy").get_all()
   def sdf = new SimpleDateFormat(format)
   sdf.setTimeZone(TimeZone.getTimeZone(CONSTS.TIMEZONE))
