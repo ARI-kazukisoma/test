409d285d372ac320898f1235bd9cec8001dbb596
masaya.toga <masaya.toga@ari-jp.com>
2018-11-13 22:00:49 +0900

    修正

---
+++ src/org/json/Util.groovy
@@ -0,0 +1,25 @@
+package org.json
+
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurperClassic
+
+Map read(filePath) {
+  readJson = readFile(file: filePath)
+  data = new JsonSlurperClassic().parseText(readJson)
+  return data
+}
+
+void write(data, filePath) {
+  json = JsonOutput.toJson(data)
+  writeFile(file: filePath, text: JsonOutput.prettyPrint(json))
+}
+
+Boolean delete(filePath) {
+  if (fileExists(filePath)) {
+    def f = new File(filePath) 
+    f.delete()
+    return true
+  }
+  return false
+}
+return this
