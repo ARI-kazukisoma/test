6c905bf69b5b37d683c9e5000bf2564e6a15b21b
masaya.toga <masaya.toga@ari-jp.com>
2018-11-13 22:07:20 +0900

    修正

---
+++ src/org/util/Constatns.groovy
@@ -0,0 +1,15 @@
+package org.slack
+
+class Constants {
+
+    public static final PLAN_CONF_FILE_NAME = [
+      'PROVISIONING': 'provisioning.json',
+      'COMMON': 'common.json',
+      'DEPLOY_APP': 'deploy_app.json',
+      'UPDATE_DATA': 'update_data.json'
+    ]
+    public static final API_URL = [
+      'APPROVAL_CREATE_PLAN_JOB': "${env.JENKINS_URL}job/開発環境新規払い出し/buildWithParameters?token=zpUcDnr5xcgppFr",
+      'REFUSAL_CREATE_PLAN_JOB': "${env.JENKINS_URL}job/開発環境新規払い出し拒否/buildWithParameters?token=n3T5kMGAVX6NCFE"
+    ]
+}
