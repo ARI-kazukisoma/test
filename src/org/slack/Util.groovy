fa65e0d6bece0fd2a32963a2f11cd19eae3b005f
masaya.toga <masaya.toga@ari-jp.com>
2018-11-13 21:33:25 +0900

    修正

--- src/org/slack/Util.groovy
+++ src/org/slack/Util.groovy
@@ -2,8 +2,8 @@ package org.slack
 
 void notifyMessage(channel, message) {
   def lib = library('auto-deploy').org.slack
-  def CONSTS = lib.Constants.CHANNEL_CREDENTIAL_IDS
-  credentialsId = CONSTS.CHANNEL_CREDENTIAL_IDS[channel]
+  credentialsId = lib.Constants.CHANNEL_CREDENTIAL_IDS[channel]
+  // credentialsId = CONSTS.CHANNEL_CREDENTIAL_IDS[channel]
   withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
     slackSend channel: channel, token: token, message: message
   }
