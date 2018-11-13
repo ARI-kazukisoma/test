c826f92bcd927a5e0aaf1c7e8414f29ee70268f1
masaya.toga <masaya.toga@ari-jp.com>
2018-11-13 20:40:05 +0900

    修正

--- src/org/slack/Util.groovy
+++ src/org/slack/Util.groovy
@@ -1,7 +1,13 @@
 package org.slack
 
-def testEcho(str) {
-  echo str
+void notifyMessage(channel, message) {
+  // CONSTS = load("constant/main.groovy").getAll()
+  // credentialsId = CONSTS.SLACK.CHANNEL_CREDENTIAL_IDS[channel]
+  credentialId = org.slack.channelCredentialIds channel
+
+  withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
+    slackSend channel: channel, token: token, message: message
+  }
 }
 
 return this
---
+++ vars/org/slack/channelCredentialIds.groovy
@@ -0,0 +1,8 @@
+// チャンネル名とJenkinsで管理しているCredentialIDを紐付ける情報
+// Slack通知に必要なトークン情報はJenkins側で管理
+def call(channel) {
+    def settings = [
+      '#hooktest': 'tmp_ari_slack_token'
+      ]
+    echo settings[channel]
+}
