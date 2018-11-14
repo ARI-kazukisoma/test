64cb7c9540522d34b82a890118526ce677c86818
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 15:37:06 +0900

    修正

--- lib/template.groovy
+++ lib/template.groovy
@@ -1,6 +1,6 @@
 String toString(fileName, binding) {
   def template_dir = "${pwd()}/template"
-  def f = new File("${template_dir}/fileName")
+  def f = new File("${template_dir}/${fileName}")
   def engine = new groovy.text.SimpleTemplateEngine()
   def template = engine.createTemplate(f).make(binding)
   return template.toString()
