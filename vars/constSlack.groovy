435d31b94a0bb24a567f9e166266a264b00be49b
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 10:15:58 +0900

    修正

--- vars/constSlack.groovy
+++
@@ -1,9 +0,0 @@
-// package constant
-// チャンネル名とJenkinsで管理しているCredentialIDを紐付ける情報
-// Slack通知に必要なトークン情報はJenkins側で管理
-def channelCredentialIds(channel) {
-    def settings = [
-      '#hooktest': 'approval_slack_token'
-      ]
-    return settings[channel]
-}
