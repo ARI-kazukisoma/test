6b214da216845f7ccae58fd257bdf69657bc2916
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 18:26:54 +0900

    修正

--- lib/lock.groovy
+++ lib/lock.groovy
@@ -10,13 +10,13 @@ def is_locked(target_env) {
 
 def do_lock(target_env) {
   dir(env.EXECUTE_LOCK_FILE_PATH) {
-    touch("${target_env.toLowerCase}.lock")
+    touch("${target_env.toLowerCase()}.lock")
   }
 }
 
 def do_unlock(target_env) {
   dir(env.EXECUTE_LOCK_FILE_PATH) {
-    sh "rm ${target_env.toLowerCase}.lock"
+    sh "rm ${target_env.toLowerCase()}.lock"
   }
 }
 
