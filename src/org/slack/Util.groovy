3414396ed3221a890a941f3c8c3b1cc0a167b237
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 10:37:59 +0900

    修正

--- src/org/slack/Util.groovy
+++ src/org/slack/Util.groovy
@@ -1,8 +1,9 @@
 package org.slack
 
 void notifyMessage(channel, message) {
-  def lib = library('auto-deploy').org.slack
-  credentialsId = lib.Constants.CHANNEL_CREDENTIAL_IDS[channel]
+  // def lib = library('auto-deploy').org.slack
+  // credentialsId = lib.Constants.CHANNEL_CREDENTIAL_IDS[channel]
+  credentialsId = "tmp_ari_slack_token"
   withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
     slackSend channel: channel, token: token, message: message
   }
