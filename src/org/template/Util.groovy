b6cd9c687f00d6225dc7427c89286bd28a286a3e
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 12:42:05 +0900

    修正

--- src/org/template/Util.groovy
+++ src/org/template/Util.groovy
@@ -4,7 +4,7 @@ String toString(template_name, binding) {
   // def f = new File(filePath)
   def engine = new groovy.text.SimpleTemplateEngine()
   def template = engine.createTemplate(text).make(binding)
-  return template.toString()
+  // return template.toString()
 }
 
 return this
