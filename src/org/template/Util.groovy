1685222ce144f1c948be9d074831466af4c615fb
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 12:56:07 +0900

    修正

--- src/org/template/Util.groovy
+++ src/org/template/Util.groovy
@@ -5,8 +5,8 @@ String getString(template_name, binding) {
   def text = libraryResource("org/template/${template_name}")
   // def f = new File(filePath)
   def engine = new groovy.text.SimpleTemplateEngine()
-  def templateClass = engine.createTemplate(text).make(binding)
-  // templateClass.toString()
+  def template = engine.createTemplate(text).make(binding)
+  templateClass.toString()
   // return template.toString()
   return "aaaa"
 }
