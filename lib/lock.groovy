ea5f190e4dd77b0292585da797c9f884714c901c
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 18:18:31 +0900

    修正

---
+++ lib/lock.groovy
@@ -0,0 +1,23 @@
+
+def is_locked(env) {
+  dir(env.EXECUTE_LOCK_FILE_PATH) {
+    if (fileExists(env.toLowerCase()) {
+      return true
+    }
+    return false
+  }
+}
+
+def do_lock(env) {
+  dir(env.EXECUTE_LOCK_FILE_PATH) {
+    touch("${env.toLowerCase}.lock")
+  }
+}
+
+def do_unlock(env) {
+  dir(env.EXECUTE_LOCK_FILE_PATH) {
+    sh "rm ${env.toLowerCase}.lock"
+  }
+}
+
+return this
