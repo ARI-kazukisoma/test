429502d513554969aa3f062ea4b6356a73bca544
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 14:03:41 +0900

    修正

--- src/org/template/Util.groovy
+++ src/org/template/Util.groovy
@@ -10,7 +10,7 @@ class Template implements Serializable {
   }
 
   String toString(template_name, binding) {
-    def text = libraryResource("org/template/${template_name}")
+    def text = this.steps.libraryResource("org/template/${template_name}")
     // def f = new File(filePath)
     def engine = new groovy.text.SimpleTemplateEngine()
     def template = engine.createTemplate(text).make(binding)
