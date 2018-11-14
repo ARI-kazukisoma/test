d155cbc7d8c84cc975dcd1d92b86f82a7438629e
masaya.toga <masaya.toga@ari-jp.com>
2018-11-14 13:29:24 +0900

    修正

--- src/org/slack/Util.groovy
+++ src/org/slack/Util.groovy
@@ -1,13 +1,21 @@
-// package org.slack
+package org.slack
 
 // // slackSendを共有ライブラリ内で実行するとエラーとなるのでここでは呼ばないようにする
 // // https://github.com/jenkinsci/slack-plugin/issues/348
 
-// // void notifyMessage(channel, message) {
-// //   def lib = library('auto-deploy').org.slack
-// //   credentialsId = lib.Constants.CHANNEL_CREDENTIAL_IDS[channel]
-// //   withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
-// //     slackSend channel: channel, token: token, message: message
-// //   }
-// // }
-// return this
+// void notifyMessage(channel, message) {
+//   def lib = library('auto-deploy').org.slack
+//   credentialsId = lib.Constants.CHANNEL_CREDENTIAL_IDS[channel]
+//   withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
+//     slackSend channel: channel, token: token, message: message
+//   }
+// }
+
+String getToken(credentialsId) {
+  def result = ''
+  withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
+    result = token
+  }
+  return token
+}
+return this
