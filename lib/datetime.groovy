14a3332ebdbc35d8895d1f862c7a80ee6463e99b
masaya.toga <masaya.toga@ari-jp.com>
2018-11-15 19:22:54 +0900

    修正

--- lib/datetime.groovy
+++ lib/datetime.groovy
@@ -6,7 +6,6 @@ TIMEZONEの設定は定数で定義されています。
 import java.text.SimpleDateFormat
 
 Long stringToUnixtime(dateString, format="yyyy/MM/dd HH:mm:ss") {
-  echo format
   def CONSTS = load("../${JOB_NAME}/constant/main.groovy").getAll()
   def sdf = new SimpleDateFormat(format)
   sdf.setTimeZone(TimeZone.getTimeZone(CONSTS.TIMEZONE))
