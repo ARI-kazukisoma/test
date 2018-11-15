e99e9282fff025723452d16eb1278755032d0ed1
masaya.toga <masaya.toga@ari-jp.com>
2018-11-15 10:34:25 +0900

    修正

--- lib/template.groovy
+++ lib/template.groovy
@@ -1,4 +1,5 @@
 String toString(fileName, binding) {
+  sh "sleep 10"
   def template_dir = "../${JOB_NAME}/template"
   def f = new File("${template_dir}/${fileName}")
   def engine = new groovy.text.SimpleTemplateEngine()
