360b0801ee2dcf272a7d46db0176c47c915fc140
masaya.toga <masaya.toga@ari-jp.com>
2018-11-15 10:37:24 +0900

    修正

--- lib/template.groovy
+++ lib/template.groovy
@@ -1,7 +1,7 @@
 String toString(fileName, binding) {
-  sh "sleep 10"
   sh "touch test.txt"
-  def f = new File("text.txt")
+  def ff = new File("text.txt")
+  sh "sleep 10"
   def template_dir = "./template"
   def f = new File("${template_dir}/${fileName}")
   def engine = new groovy.text.SimpleTemplateEngine()
