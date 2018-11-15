13e3b687587f2630c6e04d96a305ed45d593523a
masaya.toga <masaya.toga@ari-jp.com>
2018-11-15 10:39:37 +0900

    修正

--- lib/template.groovy
+++ lib/template.groovy
@@ -1,8 +1,5 @@
 String toString(fileName, binding) {
-  sh "touch test.txt"
-  def ff = new File("text.txt")
-  sh "sleep 10"
-  def template_dir = "./template"
+  def template_dir = "/var/lib/jenkins/workspace/${JOB_NAME}/template"
   def f = new File("${template_dir}/${fileName}")
   def engine = new groovy.text.SimpleTemplateEngine()
   def template = engine.createTemplate(f).make(binding)
