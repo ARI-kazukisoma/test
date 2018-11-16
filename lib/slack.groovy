4c9db1c1f97c7f1c68159e2a97f408c02e906733
masaya.toga <masaya.toga@ari-jp.com>
2018-11-16 10:42:18 +0900

    修正

--- lib/slack.groovy
+++ lib/slack.groovy
@@ -1,11 +1,15 @@
-void notifyMessage(channelTag, message) {
+void notifyMessage(channelTag, message, CONSTS=null) {
   echo "aaaaaaaaaaaaaaaa"
   echo channelTag
-  def CONSTS = load("constant/main.groovy").getAll()
-  // def (channel, credentialsId) = CONSTS.SLACK.CHANNEL_CREDENTIAL_IDS[channelTag]
+  // 呼び出す階層が深いとNotSerializableExceptioneエラーが発生するので回避策
+  // 原因はよくわかっていない
+  if (CONSTS == null) {
+    def CONSTS = load("constant/main.groovy").getAll()
+  }
+  def (channel, credentialsId) = CONSTS.SLACK.CHANNEL_CREDENTIAL_IDS[channelTag]
 
-  // withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
-  //   slackSend channel: channel, token: token, message: message
-  // }
+  withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
+    slackSend channel: channel, token: token, message: message
+  }
 }
 return this
