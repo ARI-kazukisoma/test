d626343337c6ce1ce87bd883472927c6864ca914
masaya.toga <masaya.toga@ari-jp.com>
2018-11-16 14:52:26 +0900

    修正

--- lib/template.groovy
+++ lib/template.groovy
@@ -7,23 +7,34 @@ String toString(fileName, binding) {
 }
 
 String getValidateError(errorCode, paramName) {
+  def ERROR = load("constant/error.groovy").getAll()
+  def binding = [
+    "param_name": paramName,
+    "error_message": ERROR[errorCode]
+
+  ]
+  def filePath = "${JENKINS_HOME}/workspace/${JOB_NAME}/template/error/validate.template"
+  def f = new File(filePath)
+  def engine = new groovy.text.SimpleTemplateEngine()
+  def template = engine.createTemplate(f).make(binding)
+  return template.toString()
+}
+
+String getValidateAllError(errorMessages) {
   wrap([$class: 'BuildUser']) {
     def ERROR = load("constant/error.groovy").getAll()
     def binding = [
-      "job_name": JOB_NAME,
       "user_name": BUILD_USER,
+      "job_name": JOB_NAME,
       "param_name": paramName,
-      "error_message": ERROR[errorCode]
-
+      "error_messages": ERROR[errorCode]
     ]
-    def filePath = "${JENKINS_HOME}/workspace/${JOB_NAME}/template/error/validate.template"
+    def filePath = "${JENKINS_HOME}/workspace/${JOB_NAME}/template/error/validate_all.template"
     def f = new File(filePath)
     def engine = new groovy.text.SimpleTemplateEngine()
     def template = engine.createTemplate(f).make(binding)
     return template.toString()
   }
-
-
 }
 
 return this
