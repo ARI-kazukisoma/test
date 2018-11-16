46982bbba0b4dc11dd699155ff23ecd9bf2cce1b
masaya.toga <masaya.toga@ari-jp.com>
2018-11-16 14:35:37 +0900

    修正

--- lib/slack.groovy
+++ lib/slack.groovy
@@ -6,4 +6,13 @@ def notifyMessage(channelTag, message) {
     slackSend channel: channel, token: token, message: message
   }
 }
+
+def errorMessage(channelTag, message) {
+  def CONSTS = load("constant/main.groovy").getAll()
+  def (channel, credentialsId) = CONSTS.SLACK.CHANNEL_CREDENTIAL_IDS[channelTag]
+
+  withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
+    slackSend channel: channel, token: token, message: message, color: "#FF0000"
+  }
+}
 return this
