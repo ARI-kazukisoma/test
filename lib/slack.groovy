6bb3b01fa3ba03533cc1f50c53cf76fb617fd8e3
masaya.toga <masaya.toga@ari-jp.com>
2018-11-16 17:45:13 +0900

    修正

--- lib/slack.groovy
+++ lib/slack.groovy
@@ -13,27 +13,27 @@ def errorMessage(channelTag, message) {
 }
 
 def getChannelCredential(channelTag) {
-  def CONSTS = load("constant/main.groovy").getAll()
-  return CONSTS.CHANNEL_CREDENTIAL_IDS[channelTag]
 
   /*
   本来はconstant/main.goovyで管理するべきだが、
   「def CONSTS = load("constant/main.groovy").getAll()」ロードすると
   発生条件不明の「NotSerializableException: java.io.PrintWriter」が発生するのでここで管理する
   */
+
+  settings = []
   if (env.JENKINS_ENV == 'develop') {
-    return [
-      'CHANNEL_CREDENTIAL_IDS': [
-        'admin_channel': ['#hooktest', 'tmp_ari_slack_token'],
-        'planner_channel': ['#hooktest', 'tmp_ari_slack_token']
+    settings = [
+      'admin_channel': ['#hooktest', 'tmp_ari_slack_token'],
+      'planner_channel': ['#hooktest', 'tmp_ari_slack_token']
     ]
   } else (env.JENKINS_ENV == 'production') {
-    return [
-      'CHANNEL_CREDENTIAL_IDS': [
-        'admin_channel': ['#hooktest', 'tmp_ari_slack_token'],
-        'planner_channel': ['#hooktest', 'tmp_ari_slack_token']
+    settings = [
+      'admin_channel': ['#hooktest', 'tmp_ari_slack_token'],
+      'planner_channel': ['#hooktest', 'tmp_ari_slack_token']
     ]
   }
+
+  return settings[channelTag]
 }
 
 
