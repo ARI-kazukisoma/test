6203598b2cf73e80832d061ea98a681fd1738ab2
masaya.toga <masaya.toga@ari-jp.com>
2018-11-16 16:06:55 +0900

    修正

--- lib/template.groovy
+++ lib/template.groovy
@@ -11,48 +11,52 @@ String getValidateError(errorCode, paramName) {
   def binding = [
     "param_name": paramName,
     "error_message": ERROR[errorCode]
-
   ]
   def filePath = "${JENKINS_HOME}/workspace/${JOB_NAME}/template/error/validate.template"
-  def f = new File(filePath)
-  def engine = new groovy.text.SimpleTemplateEngine()
-  def template = engine.createTemplate(f).make(binding)
-  return template.toString()
+  return toString("error/validate.templat", binding)
 }
 
 String getValidateAllError(errorMessages) {
+
+  buildUser = 'API'
+  try {
   wrap([$class: 'BuildUser']) {
-    def ERROR = load("constant/error.groovy").getAll()
-    def binding = [
-      "user_name": BUILD_USER,
-      "job_name": JOB_NAME,
-      "job_number": BUILD_NUMBER, 
-      "job_url": "${env.JENKINS_URL}job/${JOB_NAME}/${BUILD_NUMBER}/",
-      "error_messages": errorMessages
-    ]
-    def filePath = "${JENKINS_HOME}/workspace/${JOB_NAME}/template/error/validate_all.template"
-    def f = new File(filePath)
-    def engine = new groovy.text.SimpleTemplateEngine()
-    def template = engine.createTemplate(f).make(binding)
-    return template.toString()
+    try {
+      // API経由だとエラーになる
+      buildUser = BUILD_USER
+    } catch(Exception e) {}
   }
+  def ERROR = load("constant/error.groovy").getAll()
+  def binding = [
+    "user_name": buildUser,
+    "job_name": JOB_NAME,
+    "job_number": BUILD_NUMBER, 
+    "job_url": "${env.JENKINS_URL}job/${JOB_NAME}/${BUILD_NUMBER}/",
+    "error_messages": errorMessages
+  ]
+  return toString("error/validate_all.templat", binding)
 }
 String getSafetyError(errorCode) {
+
+  buildUser = 'API'
+  try {
   wrap([$class: 'BuildUser']) {
-    def ERROR = load("constant/error.groovy").getAll()
-    def binding = [
-      "user_name": BUILD_USER,
-      "job_number": BUILD_NUMBER, 
-      "job_name": JOB_NAME,
-      "job_url": "${env.JENKINS_URL}job/${JOB_NAME}/${BUILD_NUMBER}/",
-      "error_message": ERROR[errorCode]
-    ]
-    def filePath = "${JENKINS_HOME}/workspace/${JOB_NAME}/template/error/safety.template"
-    def f = new File(filePath)
-    def engine = new groovy.text.SimpleTemplateEngine()
-    def template = engine.createTemplate(f).make(binding)
-    return template.toString()
+    try {
+      // API経由だとエラーになる
+      buildUser = BUILD_USER
+    } catch(Exception e) {}
   }
+
+  def ERROR = load("constant/error.groovy").getAll()
+  def binding = [
+    "user_name": BUILD_USER,
+    "job_number": BUILD_NUMBER, 
+    "job_name": JOB_NAME,
+    "job_url": "${env.JENKINS_URL}job/${JOB_NAME}/${BUILD_NUMBER}/",
+    "error_message": ERROR[errorCode]
+  ]
+  def filePath = "${JENKINS_HOME}/workspace/${JOB_NAME}/template/error/safety.template"
+  return toString("error/safety.template", binding)
 }
 
 return this
