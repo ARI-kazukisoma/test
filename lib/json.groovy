8d5e20802fa9455d42fe4c9f8768511ad468b142
masaya.toga <masaya.toga@ari-jp.com>
2018-11-18 15:59:27 +0900

    修正

--- lib/json.groovy
+++ lib/json.groovy
@@ -20,4 +20,22 @@ Boolean delete(filePath) {
   }
   return false
 }
+
+void createTmpConfFile(data, targetEnv, fileName) {
+  json.write(common, "${env.TMP_ENV_CONFIGURATION_PATH}/${targetEnv}/${fileName}")
+}
+
+void createAllEnvConfFile(targetEnv) {
+  // 構成ファイルをtmpから正式なディレクトリにコピーする
+  dir(env.TMP_ENV_CONFIGURATION_PATH) {
+    sh "cp -pr ${targetEnv} ${env.ENV_CONFIGURATION_PATH}/"
+  }
+}
+
+void createEnvConfFile(targetEnv, fileName) {
+  dir(env.TMP_ENV_CONFIGURATION_PATH) {
+    sh "cp -p ${targetEnv}/${fileName} ${env.ENV_CONFIGURATION_PATH}/${targetEnv}/"
+  }
+
+}
 return this
