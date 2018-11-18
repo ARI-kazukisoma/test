d1b0b8d509970cb39888de4ba567c1ac851eeec1
masaya.toga <masaya.toga@ari-jp.com>
2018-11-18 14:16:33 +0900

    修正

--- src/org/datetime/Constants.groovy
+++
@@ -1,6 +0,0 @@
-package org.datetime
-
-class Constants implements Serializable{
-
-    public static final TIMEZONE = 'JST'
-}
--- src/org/slack/Constants.groovy
+++
@@ -1,8 +0,0 @@
-package org.slack
-
-class Constants implements Serializable{
-
-    public static final CHANNEL_CREDENTIAL_IDS = [
-      '#hooktest': 'tmp_ari_slack_token'
-    ]
-}
