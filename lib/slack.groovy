138ccb2b8cf4071f26b74276f4cee59927c171e1
masaya.toga <masaya.toga@ari-jp.com>
2018-11-18 13:51:50 +0900

    修正

--- lib/slack.groovy
+++ lib/slack.groovy
@@ -4,7 +4,7 @@ def notifyMessage(channelTag, message, CONSTS = null) {
   }
   def (channel, credentialsId) = CONSTS.CHANNEL_CREDENTIAL_IDS[channelTag]
 
-  // 発生条件が不明な不明のNotSerializableExceptionが発生することがあるのでtry catch()でもみ消す。
+  // 発生条件が不明なNotSerializableExceptionが発生することがあるのでtry catch()でもみ消す。
   // 送信自体はできている。
   try {
     withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
@@ -14,16 +14,18 @@ def notifyMessage(channelTag, message, CONSTS = null) {
 }
 
 def errorMessage(channelTag, message) {
-  def (channel, credentialsId) = getChannelCredential(channelTag)
-  withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
-    slackSend channel: channel, token: token, message: message, color: "#FF0000"
+  if (CONSTS == null) {
+    CONSTS = load("constant/main.groovy").getAll()
+  }
+  def (channel, credentialsId) = CONSTS.CHANNEL_CREDENTIAL_IDS[channelTag]
+
+  // 発生条件が不明なNotSerializableExceptionが発生することがあるのでtry catch()でもみ消す。
+  // 送信自体はできている。
+  try {
+    withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
+      slackSend channel: channel, token: token, message: message, color: "#FF0000"
+    }
   }
 }
-
-def getChannelCredential(channelTag) {
-  def CONSTS = load("constant/main.groovy").getAll()
-  return CONSTS.CHANNEL_CREDENTIAL_IDS[channelTag]
-}
-
 
 return this
