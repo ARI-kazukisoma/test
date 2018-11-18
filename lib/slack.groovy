4e12b887423fdd0d0759bc37796e2cf969f6ba81
masaya.toga <masaya.toga@ari-jp.com>
2018-11-18 13:43:07 +0900

    修正

--- lib/slack.groovy
+++ lib/slack.groovy
@@ -1,8 +1,16 @@
-def notifyMessage(channelTag, message) {
-  def (channel, credentialsId) = getChannelCredential(channelTag)
-  withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
-    slackSend channel: channel, token: token, message: message
+def notifyMessage(channelTag, message, CONSTS = null) {
+  if (CONSTS == null) {
+    CONSTS = load("constant/main.groovy").getAll()
   }
+  def (channel, credentialsId) = CONSTS.CHANNEL_CREDENTIAL_IDS[channelTag]
+
+  // 発生条件が不明な不明のNotSerializableExceptionが発生することがあるのでtry catch()でもみ消す。
+  // 送信自体はできている。
+  try {
+    withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
+      slackSend channel: channel, token: token, message: message
+    }
+  } catch(Exception e) {}
 }
 
 def errorMessage(channelTag, message) {
