941ccb686226e6b7581c72cda48a8302961e2f6d
masaya.toga <masaya.toga@ari-jp.com>
2018-11-20 11:44:37 +0900

    修正

--- lib/slack.groovy
+++ lib/slack.groovy
@@ -1,11 +1,11 @@
 def notifyMessage(channelTag, message, CONSTS = null) {
-  def (channel, credentialsId) = getChannelCredential(channelTag, CONSTS)
+  def (channel, credentialsId, baseUrl) = getChannelCredential(channelTag, CONSTS)
 
   // 発生条件が不明のNotSerializableExceptionが発生することがあるのでtry catch()でもみ消す。
   // 送信自体はできている。
   try {
     withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
-      slackSend baseUrl: CONSTS.SLACK_BASE_URL, channel: channel, token: token, message: message
+      slackSend baseUrl: baseUrl, channel: channel, token: token, message: message
     }
   } catch(java.io.NotSerializableException e) {}
 }
@@ -16,7 +16,7 @@ def errorMessage(channelTag, message, CONSTS = null) {
   // 送信自体はできている。
   try {
     withCredentials([string(credentialsId: credentialsId, variable: 'token')]) {
-      slackSend baseUrl: CONSTS.SLACK_BASE_URL, channel: channel, token: token, message: message, color: "#FF0000"
+      slackSend baseUrl: baseUrl, channel: channel, token: token, message: message, color: "#FF0000"
     }
   } catch(java.io.NotSerializableException e) {}
 }
@@ -26,7 +26,7 @@ def getChannelCredential(channelTag, CONSTS = null) {
     // ここのloadで発生条件が不明なNotSerializableExceptionが発生することがあるのでその際は外からCONSTSを渡している。
     CONSTS = load("constant/main.groovy").getAll()
   }
-  return CONSTS.CHANNEL_CREDENTIAL_IDS[channelTag]
+  return CONSTS.CHANNEL_CREDENTIAL_IDS[channelTag] + [CONSTS.SLACK_BASE_URL]
 }
 
 
